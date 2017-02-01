23a5088755ed9b517b59a0c2d4762ebde8df0538
marcusljx <marcusljx@gmail.com>
2017-02-02 00:00:44 +0800

    adding prelim files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # eventium
+[ ![Codeship Status for marcusljx/eventium](https://app.codeship.com/projects/bba27130-cac2-0134-cb7a-32781b3b5662/status?branch=master)](https://app.codeship.com/projects/199696)
+
 Event-timeline collation
---
+++ server/server_logic_test.go
@@ -0,0 +1,15 @@
+package server
+
+import "testing"
+
+func TestEventiumLogic_GetEventByID(t *testing.T) {
+	return
+}
+
+func TestEventiumLogic_GetEventByNearbyLocation(t *testing.T) {
+	return
+}
+
+func TestEventiumLogic_PostEvent(t *testing.T) {
+	return
+}
